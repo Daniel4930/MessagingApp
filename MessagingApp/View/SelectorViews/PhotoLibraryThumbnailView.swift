a4f4d4c315b2f836e45fdad5ad2d68375eb05265
Daniel Le <phule4108@gmail.com>
2025-09-20 12:38:43 -0700

    Organized photo library thumbnail view for readability

--- MessagingApp/View/SelectorViews/PhotoLibraryThumbnailView.swift
+++ MessagingApp/View/SelectorViews/PhotoLibraryThumbnailView.swift
@@ -11,15 +11,12 @@ import PhotosUI
 struct PhotoLibraryThumbnailView: View {
     let asset: PHAsset?
     @ObservedObject var messageComposerViewModel: MessageComposerViewModel
-    @State private var uploadedFile: UploadedFile? = nil
+    @StateObject var photoLibraryVM = PhotoLibraryViewModel()
     @State private var dataExistInSelection = false
-    
-    let frame: (width: CGFloat, height: CGFloat) = (120, 120)
-    let newOpacity: CGFloat = 0.3
     
     var body: some View {
         ZStack {
-            if let uploadData = uploadedFile {
+            if let uploadData = photoLibraryVM.uploadedFile {
                 Button {
                     if messageComposerViewModel.checkDataExist(identifier: uploadData.identifier) {
                         messageComposerViewModel.removeData(identifier: uploadData.identifier)
@@ -38,89 +35,22 @@ struct PhotoLibraryThumbnailView: View {
             } else {
                 RoundedRectangle(cornerRadius: 10)
                     .fill(Color.gray.opacity(0.3))
-                    .frame(width: frame.width, height: frame.height)
+                    .frame(width: photoLibraryVM.frame.width, height: photoLibraryVM.frame.height)
                     .overlay(
                         ProgressView()
                     )
             }
         }
         .task {
-            await loadPhotoOrVideoFile()
+            await photoLibraryVM.loadPhotoOrVideoFile(asset: asset)
         }
         .onChange(of: messageComposerViewModel.selectionData) { oldValue, newValue in
-            if let data = uploadedFile {
+            if let data = photoLibraryVM.uploadedFile {
                 if messageComposerViewModel.checkDataExist(identifier: data.identifier) {
                     dataExistInSelection = true
                 }
                 else {
                     dataExistInSelection = false
-                }
-            }
-        }
-    }
-}
-extension PhotoLibraryThumbnailView {    
-    func loadPhotoOrVideoFile() async {
-        if let asset = asset {
-            let size = CGSize(width: frame.width * UIScreen.main.scale, height: frame.height * UIScreen.main.scale)
-            
-            let requestImageOptions = PHImageRequestOptions()
-            requestImageOptions.deliveryMode = .highQualityFormat
-            requestImageOptions.isNetworkAccessAllowed = true
-            
-            let videoRequestOptions = PHVideoRequestOptions()
-            requestImageOptions.deliveryMode = .highQualityFormat
-            videoRequestOptions.isNetworkAccessAllowed = true
-            
-            if asset.mediaType == .image {
-                guard let uIImage = await messageComposerViewModel.requestImageFromAsset(asset: asset, size: size, options: requestImageOptions) else { return }
-                let name = (asset.value(forKey: "filename") as? String) ?? UUID().uuidString + ".png"
-                uploadedFile = UploadedFile(
-                    identifier: asset.localIdentifier,
-                    fileType: .photo,
-                    photoInfo: PhotoFile(name: name, image: uIImage),
-                    videoInfo: nil,
-                    fileInfo: nil
-                )
-            }
-            else if asset.mediaType == .video {
-                let name = (asset.value(forKey: "filename") as? String) ?? UUID().uuidString + ".mp4"
-                PHImageManager.default().requestAVAsset(forVideo: asset, options: videoRequestOptions) { (avAsset, _, _) in
-                    guard let urlAsset = avAsset as? AVURLAsset else {
-                        print("ERROR: Unable to convert avAsset to urlAsset")
-                        return
-                    }
-                    let generator = AVAssetImageGenerator(asset: avAsset!)
-                    generator.appliesPreferredTrackTransform = true
-                    generator.maximumSize = size
-
-                    Task {
-                        do {
-                            let cgImage = try await generator.image(at: CMTime.zero).image
-                            let uiImage = UIImage(cgImage: cgImage)
-
-                            let duration = try await urlAsset.load(.duration)
-                            let durationSeconds = CMTimeGetSeconds(duration)
-                            
-                            let videoURL = urlAsset.url
-                            let videoData = try Data(contentsOf: videoURL)
-                            
-                            uploadedFile = UploadedFile(
-                                identifier: asset.localIdentifier,
-                                fileType: .video,
-                                photoInfo: nil,
-                                videoInfo: VideoFile(
-                                    name: name,
-                                    duration: durationSeconds,
-                                    videoData: videoData,
-                                    thumbnail: uiImage
-                                ),
-                                fileInfo: nil
-                            )
-                        } catch {
-                            print("Failed to generate video thumbnail: \(error.localizedDescription)")
-                        }
-                    }
                 }
             }
         }
